6c59d581e4d18531917f55cb1856f2fa998b4ec6
kiran <kiran>
2024-12-19 16:32:11 +1100

    Re-added files to track

---
+++ GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,15 @@
+using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateSubscription(CreateSubscriptionRequest request) => Ok(request);
+    
+    [HttpGet]
+    public IActionResult GetSubscriptions() => Ok();
+}
