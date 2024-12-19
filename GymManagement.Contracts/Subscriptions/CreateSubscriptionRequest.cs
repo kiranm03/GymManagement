fe0f1db87a2d93955e6e6a168a88bbbbb0b79978
kiran <kiran>
2024-12-19 16:32:00 +1100

    Removed all files from Git index

--- GymManagement.Contracts/Subscriptions/CreateSubscriptionRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace GymManagement.Contracts.Subscriptions;
-
-public record CreateSubscriptionRequest(Guid AdminId, SubscriptionType SubscriptionType);
--- GymManagement.Contracts/Subscriptions/SubscriptionResponse.cs
+++
@@ -1,3 +0,0 @@
-namespace GymManagement.Contracts.Subscriptions;
-
-public record SubscriptionResponse(Guid Id, SubscriptionType SubscriptionType);
